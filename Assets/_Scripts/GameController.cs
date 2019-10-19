77782911df14bd24d2c393adb11ea44352bccdc8
Jinkyu Choi <jchoi123@my.centennialcollege.ca>
2019-10-19 15:21:23 -0400

    Copied Example and done Task 1

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Scene Game Objects")]
+    public GameObject cloud;
+    public GameObject island;
+    public int numberOfClouds;
+    public List<GameObject> clouds;
+
+    [Header("Audio Sources")]
+    public SoundClip activeSoundClip;
+    public AudioSource[] audioSources;
+
+    [Header("Scoreboard")]
+    [SerializeField]
+    private int _lives;
+
+    [SerializeField]
+    private int _score;
+
+    public Text livesLabel;
+    public Text scoreLabel;
+    public Text highScoreLabel;
+
+    public GameObject scoreBoard;
+
+    private int currentLives;
+    private int currentScore;
+
+    [Header("UI Control")]
+    public GameObject startLabel;
+    public GameObject startButton;
+    public GameObject endLabel;
+    public GameObject restartButton;
+
+    // public properties
+    public int Lives
+    {
+        get
+        {
+            return _lives;
+        }
+
+        set
+        {
+            _lives = value;
+            scoreBoard.GetComponent<HighScore>().lives = _lives;
+
+            if (_lives < 1)
+            {
+                
+                SceneManager.LoadScene("End");
+            }
+            else
+            {
+                livesLabel.text = "Lives: " + _lives.ToString();
+            }
+           
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+
+        set
+        {
+            _score = value;
+            scoreBoard.GetComponent<HighScore>().score = _score;
+
+
+            if (scoreBoard.GetComponent<HighScore>().highScore < _score)
+            {
+                scoreBoard.GetComponent<HighScore>().highScore = _score;
+            }
+            scoreLabel.text = "Score: " + _score.ToString();
+
+            if(_score >= 500)
+            {
+                SceneManager.LoadScene("Level2");
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObjectInitialization();
+        SceneConfiguration();
+    }
+
+    private void GameObjectInitialization()
+    {
+        scoreBoard = GameObject.Find("HighScore");
+
+        startLabel = GameObject.Find("StartLabel");
+        endLabel = GameObject.Find("EndLabel");
+        startButton = GameObject.Find("StartButton");
+        restartButton = GameObject.Find("RestartButton");
+    }
+
+
+    private void SceneConfiguration()
+    {
+
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Start":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                highScoreLabel.enabled = false;
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.NONE;
+                break;
+            case "Main":
+                Lives = 5;
+                Score = 0;
+                highScoreLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.ENGINE;
+                break;
+            case "Level2":
+                Lives = scoreBoard.GetComponent<HighScore>().lives;
+                Score = scoreBoard.GetComponent<HighScore>().score;
+                highScoreLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.ENGINE;
+                break;
+            case "End":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                activeSoundClip = SoundClip.NONE;
+                highScoreLabel.text = "High Score: " + scoreBoard.GetComponent<HighScore>().score;
+                break;
+        }
+
+
+        if ((activeSoundClip != SoundClip.NONE) && (activeSoundClip != SoundClip.NUM_OF_CLIPS))
+        {
+            AudioSource activeAudioSource = audioSources[(int)activeSoundClip];
+            activeAudioSource.playOnAwake = true;
+            activeAudioSource.loop = true;
+            activeAudioSource.volume = 0.5f;
+            activeAudioSource.Play();
+        }
+
+
+
+        // creates an empty container (list) of type GameObject
+        clouds = new List<GameObject>();
+
+        for (int cloudNum = 0; cloudNum < numberOfClouds; cloudNum++)
+        {
+            clouds.Add(Instantiate(cloud));
+        }
+
+        Instantiate(island);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    // Event Handlers
+    public void OnStartButtonClick()
+    {
+        DontDestroyOnLoad(scoreBoard);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Assets/_Scripts/OceanController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OceanController : MonoBehaviour
+{
+    public float verticalSpeed = 0.1f;
+    public float horizontalSpeed = 0.1f;
+    public float resetPosition = 4.8f;
+    public float resetPoint = -4.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneConfiguration();
+        CheckBounds();
+    }
+
+    void SceneConfiguration()
+    {
+
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Main":
+                VerticleMove();
+                break;
+            case "Level2":
+                HorizontalMove();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// This method moves the ocean down the screen by verticalSpeed
+    /// </summary>
+    void VerticleMove()
+    {
+        Vector2 newPosition = new Vector2(0.0f, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    void HorizontalMove()
+    {
+        Vector2 newPosition = new Vector2(horizontalSpeed, 0.0f);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    /// <summary>
+    /// This method resets the ocean to the resetPosition
+    /// </summary>
+    void Reset()
+    {
+        transform.position = new Vector2(0.0f, resetPosition);
+    }
+
+    /// <summary>
+    /// This method checks if the ocean reaches the lower boundary
+    /// and then it Resets it
+    /// </summary>
+    void CheckBounds()
+    {
+        if (transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
